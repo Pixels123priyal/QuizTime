5eb7571382b6bee476429a28de3004c0d2671d67
harsh-7578 <harshsingh7578@gmail.com>
2021-11-22 12:44:02 +0530

    final layout

--- src/App.js
+++ src/App.js
@@ -1,16 +1,19 @@
-
+import Bounce from 'react-reveal/Bounce';
 import './App.css';
-import Header from './Components/Header/Header'
-import Footer from './Components/Footer/Footer';
-import CenterContent from './Components/CenterContent/CenterContent'
+import QuizData from './Components/QuizData';
 function App() {
   
   return (
     <div className="App" >
      
-        <Header/>
-        <CenterContent/>
-    <Footer/>
+      <div className="Main-Heading"><h1>QUIZ TIME</h1></div>
+      <div className="Message">
+        <h2>
+          <Bounce left cascade>Unleash The Knowledge Within You
+            </Bounce>
+          </h2>
+      </div>
+      <QuizData/>
     </div>
   );
 }
