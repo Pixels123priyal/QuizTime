576376804aadbacaa0910684b81bfe1b3783d628
Pixels123priyal <85298332+Pixels123priyal@users.noreply.github.com>
2021-11-27 00:22:43 +0530

    Add files via upload

---
+++ src/Components/Button.js
@@ -0,0 +1,24 @@
+import { PropTypes } from "prop-types";
+
+const Button = ({ color, text,onClick }) => {
+
+
+    return (<div> 
+        <button className='btn'
+        style={{ backgroundColor: color }}
+        onClick={onClick}>{text}</button>
+        
+    </div> )
+}
+
+Button.defaultProps = {
+    color: 'red',
+    // alert:"Are you Sure ?"
+}
+
+Button.propTypes = {
+    text: PropTypes.string,
+    color: PropTypes.string,
+    onClick:PropTypes.func,
+}
+export default Button
