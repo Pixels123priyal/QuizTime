86217678a0b9c1765cc9acb5c31f2bba6aade9c1
Pixels123priyal <85298332+Pixels123priyal@users.noreply.github.com>
2022-01-20 12:52:21 +0530

    source files
    QuizTime_Beginners

---
+++ questions.js
@@ -0,0 +1,60 @@
+// questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "Name of the first Atomic Submarine of India ?    ",
+    answer: "I.N.S Chakra",
+    options: [
+      "I.N.S Chakra",
+      "R.N. Shukla",
+      "V.R. Gill",
+      "D.B. Mahawar"
+    ]
+  },
+    {
+    numb: 2,
+    question: "What is the name of first British to visit India? ",
+    answer: "Hawkins",
+    options: [
+      "Hawkins",
+      "Norway",
+      "Devid",
+      "George Bush"
+    ]
+  },
+    {
+    numb: 3,
+    question: "Name of the first university of India?",
+    answer: "Nalanda University",
+    options: [
+      "Nalanda University",
+      "Taxshila University",
+      "Jawahar University",
+      "Dronacharya University"
+    ]
+  },
+    {
+    numb: 4,
+    question: "Where was the first Post Office opened in India?",
+    answer: "Kolkata in 1727",
+    options: [
+      "Kolkata in 1727",
+      "Asaam in 1827",
+      "Madras in 1928",
+      "Delhi in 1230"
+    ]
+  },
+    {
+    numb: 5,
+    question: "Name of the first election commissioner of India?",
+    answer: "Sukumar Sen",
+    options: [
+      "Sukumar Sen",
+      "R.N. Shukla",
+      "V.R. Gill",
+      "D.B. Mahawar"
+    ]
+  },
+ 
+];
+
